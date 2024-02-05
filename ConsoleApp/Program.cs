631de8bdec91b35ac9cd901678ca32d2bd991f00
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-02-05 20:35:40 +0300

    ConsoleApp ile Person nesnesi new'lendi. PttManager kendi içinde PersonManager kullandı. GiveMask metodu ile kimlik doğrulaması gerçekleştirildi.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person()
+{
+    FirstName = "Hüseyin",
+    LastName = "Aydın",
+    DateOfBirthYear = 1994,
+    NationalIdentity = 00000000000 //Yok yok (-:
+};
+
+PttManager pttManager = new PttManager(new PersonManager());
+await pttManager.GiveMask(person1);
