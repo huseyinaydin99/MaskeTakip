d13d9c091c91af94d3f2818b99948ca9b4b062d7
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-02-05 20:27:14 +0300

    Türkiye Cumhuriyeti İçişleri Bakanlığı Nüfus teşkilatı Nüfus ve Vatandaşlık İşleri Genel Müdürlüğünün Mernis SOAP Web Servisleri sistemi eklendi ve PersonManager sınıfı iş mantığında kullanıcı gerçek bir kullanıcı mı diye kontrol edildi.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PersonManager : IApplicantService
+{
+
+    public List<Person> GetList()
+    {
+        return null;
+    }
+
+    public async Task<bool> CheckPersonAsync(Person person)
+    {
+        KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+        var dogrulaResult = await client.TCKimlikNoDogrulaAsync
+        (TCKimlikNo: person.NationalIdentity,
+        Ad: person.FirstName,
+        Soyad: person.LastName,
+        DogumYili: person.DateOfBirthYear);
+        return dogrulaResult.Body.TCKimlikNoDogrulaResult;
+    }
+
+    public void ApplyForMask(Person person)
+    {
+
+    }
+}
