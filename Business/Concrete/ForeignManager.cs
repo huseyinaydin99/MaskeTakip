206180955a34917270c3d757b51e795a8afba7a8
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-02-05 20:21:50 +0300

    ForeignManager sınıfı iş katmanına yazıldı.

---
+++ Business/Concrete/ForeignManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class ForeignManager : IApplicantService
+{
+    public void ApplyForMask(Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CheckPersonAsync(Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Person> GetList()
+    {
+        throw new NotImplementedException();
+    }
+}
